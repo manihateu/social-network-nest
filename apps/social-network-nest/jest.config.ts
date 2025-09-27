889ebb5dd6df74db431df77b3d52600b43dd08cb
Nickita <102178609+manihateu@users.noreply.github.com>
2025-09-27 03:24:19 +0300

    Initial commit

---
+++ apps/social-network-nest/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'social-network-nest',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/social-network-nest',
+};
